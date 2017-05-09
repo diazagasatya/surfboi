91fefc5a33e56d3423cf143064db60b21fb389e9
Diaz Agasatya <diazagasatya@gmail.com>
2017-05-09 11:05:14 -0700

    The original commit for the password project

---
+++ Assignment_1/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   cout << "Hello World!\n\n";
+   cout << "My family name is Agasatya" << endl;
+   cout << "My student ID is 20079032" << endl << endl;
+   cout << "[Syllabus #1: Other IDE are welcome, however assistance will\n"
+      << "focus on Xcode, Visual X, and Eclipse.]" << endl << endl;
+   cout << "[Syllabus #2: STEM Success Center is utilize for guidance and\n"
+      << "assistance, but not for fast solution giver.]" << endl << endl;
+   cout << "[Specific Lab #1: Simplicity is key. Limit the unnecessary work\n"
+      << "for the computer and ourselves.] " << endl << endl;
+   cout << "[Specific Lab #2: Use descriptive names for variables like celcius\n"
+      << "or cityJava. Do not use cryptic names like z or a.]" << endl << endl;
+   cout << "[Specific Lab #3: Finishing early gives us the time review and\n"
+      << "modify our work if there's beneficial discussion in forum or STEM\n"
+      << "success center. This is due to rejection of multiple submission.]"
+      << endl << endl;
+
+   return 0;
+}
+
+/*--------------------------------Paste of Run----------------------------------
+Hello World!
+
+My family name is Agasatya
+My student ID is 20079032
+
+[Syllabus #1: Other IDE are welcome, however assistance will
+focus on Xcode, Visual X, and Eclipse.]
+
+[Syllabus #2: STEM Success Center is utilize for guidance and
+assistance, but not for fast solution giver.]
+
+[Specific Lab #1: Simplicity is key. Limit the unnecessary work
+for the computer and ourselves.] 
+
+[Specific Lab #2: Use descriptive names for variables like celcius
+or cityJava. Do not use cryptic names like z or a.]
+
+[Specific Lab #3: Finishing early gives us the time review and
+modify our work if there's beneficial discussion in forum or STEM
+success center. This is due to rejection of multiple submission.]
+
+Program ended with exit code: 0
+--------------------------------End of Assignment 1---------------------------*/
